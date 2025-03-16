c15e6c5e18c4bcfbae1ac041cc36d828c8dbcf2f
Sergo Tsoko <cokolenko.sergo@gmail.com>
2025-03-16 19:24:23 +0200

    firs

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,70 @@
 <script>
 import ElementPlus from 'element-plus';
 
-export default{
-  data(){
+export default {
+  data() {
     return {
       number: 0,
-      text: "Тут написан очень важный текст"
-    }
+      text: "Тут написан очень важный текст",
+      isTextVisible: true // добавляем переменную для управления видимостью
+    };
   },
   methods: {
-    addNumber(){
-      this.number += 1
+    addNumber() {
+      this.number += 1;
     },
-    removeNumber(){
-      this.number -= 1
+    removeNumber() {
+      this.number -= 1;
     },
-    hideText(){
-    this.text = "                               ";
-    addEventListener
-    },
-    showText(){
-      this.text = "Тут написан очень важный текст"
+    toggleText() {
+      this.isTextVisible = !this.isTextVisible; // переключаем состояние
     }
   }
-  
-
-}
-
+};
 </script>
 
 <template>
-<div class="number">
-    <!-- <button @click="addNumber">+</button> -->
+  <div class="number">
     <el-button type="primary" @click="addNumber">+</el-button>
-
-    <!-- <button v-on:click="removeNumber">-</button> -->
     <el-button type="primary" @click="removeNumber">-</el-button>
-    
   </div>
+  
   <div class="cifra">
     Ваша цифра: <em>{{ number }}</em> 
   </div>
 
   <div>
     <div class="text1">
-     
-      
-      <!-- <button @click="hideText">скрыть текст</button> -->
-      <!-- <button @click="hideText">скрыть текст</button> -->
-      <el-button type="primary" @click="hideText">скрыть текст</el-button>
-      <el-button type="primary" @click="showText">показать текст</el-button>
-
-      <!-- <button @click="showText">показать текст</button> -->
-      
+      <el-button type="primary" @click="toggleText">
+        {{ isTextVisible ? "Скрыть текст" : "Показать текст" }}
+      </el-button>
     </div>
-    <div class="samtext">
 
-      {{text}}
+    <div class="samtext" v-if="isTextVisible">
+      {{ text }}
     </div>
   </div>
 </template>
 
 <style scoped>
-.number{
+.number {
   margin-left: 40%;
   padding-top: 50px;
-
 }
-.cifra{
+.cifra {
   margin-left: 40%;
-  
 }
-.text1{
+.text1 {
   margin-left: 40%;
   display: flex;
   flex-flow: column;
   width: fit-content;
 }
-.samtext{
+.samtext {
   justify-content: center;
   display: flex;
 }
-
-.el-button+.el-button{
+.el-button + .el-button {
   margin: 5px;
 }
-
 </style>
